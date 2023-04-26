5883d40c50283718255fc2c47f5579a18129ebe6
zakoden <59803738+zakoden@users.noreply.github.com>
2023-04-26 22:11:55 +0300

    Added option for random crop

--- train.py
+++ train.py
@@ -41,6 +41,7 @@ def setup_training_loop_kwargs(
     cond       = None, # Train conditional model based on dataset labels: <bool>, default = False
     subset     = None, # Train with only N images: <int>, default = all
     mirror     = None, # Augment dataset with x-flips: <bool>, default = False
+    rcrop      = None, # Size for random crop: <int>, default = no crop
 
     # Base config.
     cfg        = None, # Base config: 'auto' (default), 'stylegan2', 'paper256', 'paper512', 'paper1024', 'cifar'
@@ -98,13 +99,13 @@ def setup_training_loop_kwargs(
     assert isinstance(seed, int)
     args.random_seed = seed
 
-    # -----------------------------------
-    # Dataset: data, cond, subset, mirror
-    # -----------------------------------
+    # ------------------------------------------
+    # Dataset: data, cond, subset, mirror, rcrop
+    # ------------------------------------------
 
     assert data is not None
     assert isinstance(data, str)
-    args.training_set_kwargs = dnnlib.EasyDict(class_name='training.dataset.ImageFolderDataset', path=data, use_labels=True, max_size=None, xflip=False)
+    args.training_set_kwargs = dnnlib.EasyDict(class_name='training.dataset.ImageFolderDataset', path=data, use_labels=True, max_size=None, xflip=False, rcrop=rcrop)
     args.data_loader_kwargs = dnnlib.EasyDict(pin_memory=True, num_workers=3, prefetch_factor=2)
     try:
         training_set = dnnlib.util.construct_class_by_name(**args.training_set_kwargs) # subclass of training.dataset.Dataset
@@ -141,6 +142,9 @@ def setup_training_loop_kwargs(
     if mirror:
         desc += '-mirror'
         args.training_set_kwargs.xflip = True
+        
+    if rcrop is not None:
+      desc += f'-rcrop{rcrop}'
 
     # ------------------------------------
     # Base config: cfg, gamma, kimg, batch
@@ -411,6 +415,7 @@ class CommaSeparatedList(click.ParamType):
 @click.option('--cond', help='Train conditional model based on dataset labels [default: false]', type=bool, metavar='BOOL')
 @click.option('--subset', help='Train with only N images [default: all]', type=int, metavar='INT')
 @click.option('--mirror', help='Enable dataset x-flips [default: false]', type=bool, metavar='BOOL')
+@click.option('--rcrop', help='Size of random crop [default: no crop]', type=int, metavar='INT')
 
 # Base config.
 @click.option('--cfg', help='Base config [default: auto]', type=click.Choice(['auto', 'stylegan2', 'paper256', 'paper512', 'paper1024', 'cifar']))
@@ -510,6 +515,7 @@ def main(ctx, outdir, dry_run, **config_kwargs):
     print(f'Image resolution:   {args.training_set_kwargs.resolution}')
     print(f'Conditional model:  {args.training_set_kwargs.use_labels}')
     print(f'Dataset x-flips:    {args.training_set_kwargs.xflip}')
+    print(f'Random crop size:   {args.training_set_kwargs.rcrop}')
     print()
 
     # Dry run?
