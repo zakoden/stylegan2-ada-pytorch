c02aa05aac057d07f6df90997711783e03f5c51d
zakoden <59803738+zakoden@users.noreply.github.com>
2023-04-28 19:11:42 +0300

    Update train.py

--- train.py
+++ train.py
@@ -88,8 +88,7 @@ def setup_training_loop_kwargs(
     args.network_snapshot_ticks = snap
 
     if metrics is None:
-        metrics = []
-        #metrics = ['fid50k_full']
+        metrics = ['fid50k_full']
     assert isinstance(metrics, list)
     if not all(metric_main.is_valid_metric(metric) for metric in metrics):
         raise UserError('\n'.join(['--metrics can only contain the following values:'] + metric_main.list_valid_metrics()))
