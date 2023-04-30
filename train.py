a6b7f4dba845345944cc7fa250b8521ac0299b9c
zakoden <59803738+zakoden@users.noreply.github.com>
2023-04-30 16:15:35 +0300

    Update train.py

--- train.py
+++ train.py
@@ -284,6 +284,7 @@ def setup_training_loop_kwargs(
         'bgcf':   dict(xflip=1, rotate90=1, xint=1, scale=1, rotate=1, aniso=1, xfrac=1, brightness=1, contrast=1, lumaflip=1, hue=1, saturation=1, imgfilter=1),
         'bgcfn':  dict(xflip=1, rotate90=1, xint=1, scale=1, rotate=1, aniso=1, xfrac=1, brightness=1, contrast=1, lumaflip=1, hue=1, saturation=1, imgfilter=1, noise=1),
         'bgcfnc': dict(xflip=1, rotate90=1, xint=1, scale=1, rotate=1, aniso=1, xfrac=1, brightness=1, contrast=1, lumaflip=1, hue=1, saturation=1, imgfilter=1, noise=1, cutout=1),
+        'pixel':  dict(xflip=1, rotate90=1, xint=1, brightness=1, contrast=1, lumaflip=1, hue=1, saturation=1),
     }
 
     assert augpipe in augpipe_specs
